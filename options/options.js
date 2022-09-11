24617c169dd85c76aa4a033f7e676d97b1a1d18b
Crowdbotics <team@crowdbotics.com>
2022-09-11 15:51:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "live_cricket_tv_hd_37045",
+  url: "https://live_cricket_tv_hd_37045.botics.co",
+  api: "https://live_cricket_tv_hd_37045.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
